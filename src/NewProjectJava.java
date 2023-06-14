fea5ee67b498e028fa6e25909d039632fa5f4579
Tanya <Tanja_33@mail.ru>
2023-06-14 12:49:18 +0700

    Gitignore added

---
+++ src/NewProjectJava.java
@@ -0,0 +1,15 @@
+public class NewProjectJava {
+    public static void main(String[] args) {
+        int price = 12500;
+        int amount = 20;
+
+        int miles = price / amount;
+
+        System.out.println("Количество начисленных миль:" + miles);
+
+
+
+
+
+    }
+}
